be8199413a40051f49fadeb9afd92b9ce574568b
alex <nba4everwanted@gmail.com>
2023-02-07 16:48:14 +0000

    Expense Tracker and Budget app using React toolkit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Transactions from './features/transactions/Transactions'
+import Budgets from './features/budgets/Budgets';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Expense Tracker</h1>
+      <Budgets />
+      <Transactions />
       </header>
     </div>
   );
